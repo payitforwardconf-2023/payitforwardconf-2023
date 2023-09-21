ee02684053fc85c3cbefa23a6def920b5b13d196
mori8 <kaithape@gmail.com>
2023-09-21 23:13:19 +0900

    implement: main section

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,27 @@ export default function App({}: Props) {
     <div id="wrap" className="font-pretendard">
       <Navigator />
       <div id="container">
-        <div id="main" className=""></div>
+        <div
+          id="main"
+          className="w-full h-screen bg-violet-50 bg-main bg-cover bg-center flex flex-col justify-between py-32 px-14"
+        >
+          <div>
+            <h1 className="font-proxima font-black text-7xl text-white">
+              1<sup>st</sup> SSHS
+              <br/>
+              PAY IT FORWARD
+              <br />
+              CONFERENCE
+            </h1>
+          </div>
+          <div>
+            <h2 className="font-proxima font-black text-4xl text-white leading-10">
+              2023 OCTOBER 28 (SAT)
+              <br />
+              10:00 - 18:00 <span className="font-pretendard">@서울과학고등학교</span>
+            </h2>
+          </div>
+        </div>
         <SectionLayout
           sectionId="session-info"
           title={`SESSION\nINFORMATION`}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        main: 'url("/public/pay-it-forward-conf-bg-contrast.png")',
+      },
+    },
     fontFamily: {
-      'proxima': ['Proxima Nova', 'Poppins', 'sans-serif'],
-      'pretendard': ['Pretendard', 'Noto Sans KR', 'sans-serif'],
-    }
+      proxima: ["Proxima Nova", "Poppins", "sans-serif"],
+      pretendard: ["Pretendard", "Noto Sans KR", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+};
